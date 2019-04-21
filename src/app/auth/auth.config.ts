a5dea17fde853937a3c385e7662c9dde549b6f65
vikaspulluri <vikasiiitn@gmail.com>
2019-04-21 15:42:35 +0530

    Update google auth client key

--- src/app/auth/auth.config.ts
+++ src/app/auth/auth.config.ts
@@ -1,6 +1,6 @@
 export const authConfig = {
     googleAuthConfig: {
-        client_id: '401384923720-kn9j2otath5qr0uj7rmmk3vsfarj2jdl.apps.googleusercontent.com',
+        client_id: '401384923720-burljeegholn8kkih169vn0jcnvr8344.apps.googleusercontent.com',
         cookiepolicy: 'single_host_origin',
         scope: 'profile email'
     }
